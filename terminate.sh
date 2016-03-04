103b6b364afebec654f8adf3bbb0af8a0fd54378
patricksimpson <izerop@gmail.com>
2016-03-04 12:24:05 -0500

    feat: adding the terminator

---
+++ terminate.sh
@@ -0,0 +1,25 @@
+if [ -z "$1" ]; then
+  printf 'What PORT would you like to terminate: '
+  read port
+else
+  port=$1
+fi
+
+if [ $port ]; then
+
+  output=$(lsof -i tcp:$port);
+  lsof -i tcp:$port
+
+  if [ ! -z "$output" ]; then
+    read -p "Do you wish to terminate these programs? (Y/n)" -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      lsof -P | grep ":$port" | awk '{print $2}' | xargs kill -9
+      echo 'Hasta la vista'
+    fi
+  else
+    echo 'Nothing to kill here...'
+  fi
+  echo 'Bye.'
+  exit 1
+fi
